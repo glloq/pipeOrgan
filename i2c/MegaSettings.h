9c73041338e125c90f857bab61c60b863cb1cf7d
glloq <glloq.nz@gmail.com>
2024-12-04 23:55:21 +0100

    Update MegaSettings.h

--- i2c/MegaSettings.h
+++ i2c/MegaSettings.h
@@ -1,31 +1,38 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <Arduino.h>
-
-// I2C Slave Address
-#define I2C_ADDRESS 8
-
-// Default velocity for Note On messages
-#define DEFAULT_VELOCITY 100
-
-// Number of keys on the keyboard
+// Nombre total d'inputs
 #define NUM_NOTES 54
 
-// Pins for each key (digital inputs)
-const uint8_t notePins[NUM_NOTES] = {
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-    34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
-    46, 47, 48, 49, 50, 51, 52, 53, A0, A1, A2, A3, A4, A5
+// Structure pour regrouper tous les paramètres pour chaque touche
+typedef struct {
+  byte midiChannel;     // Canal MIDI (0-15)
+  int pin;              // Pin associée à la touche (entrée numérique)
+  byte messageType;     // Type de message MIDI (0 pour Note, 1 pour Program Change)
+  byte param1;          // Paramètre 1 (Numéro de la note MIDI ou valeur pour activer un PC)
+  byte param2;          // Paramètre 2 (Utilisé pour désactiver un PC)
+} MidiInput;
+
+
+// put each inputs here 
+MidiInput midiInput[NUM_NOTES] = {
+  // Format : {channel, pin, messageType (note=0 or PC=1) , param1, param2}
+
+  // Exemple  message Note for MIDI 60 (C4)
+  { 0, 2, 0, 60, 0 },  // Channel 0, Pin 2, Note (MIDI 60 - C4), the last one is not used for a note
+
+  // Exemple pour un message Program Change
+  { 0, 3, 1, 0, 1 },   // Channel 0, Pin 3, Program Change (param1 = 0 midi ONn, param2 = 1 Midi OFF)=> bourdon 16' on your pipe organ
+  { 0, 4, 1, 2, 3 },   // Channel 0, Pin 3, Program Change (param1 = 2 midi ONn, param2 = 3 Midi OFF)=> montre 8' on your pipe organ
+
+  // Ajouter les autres notes avec les mêmes paramètres (canal, pin, messageType, param1, param2)
 };
 
-// Corresponding MIDI note numbers
-const uint8_t noteMidiNumbers[NUM_NOTES] = {
-    36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-    48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
-    72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85
-};
 
-#endif
+// Adresse de l'esclave I2C (Arduino Mega)
+#define SLAVE_ADDRESS 8
+
+// Intervalle de balayage du clavier (en ms)
+#define DEBOUNCE_TIME 5
+
+#endif // SETTINGS_H
