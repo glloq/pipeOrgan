36d83a40d0505ab37b9e6370b3b26437cc46b0d4
glloq <glloq.nz@gmail.com>
2024-12-04 19:08:33 +0100

    Create microMaster.ino

---
+++ i2c/microMaster.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <MIDIUSB.h>
+
+// I2C Slave Addresses for the Mega devices
+#define MEGA1_ADDRESS 8
+#define MEGA2_ADDRESS 9
+#define MEGA3_ADDRESS 10
+
+void setup() {
+    // Initialize I2C as master
+    Wire.begin();
+    Serial.begin(115200);
+}
+
+void loop() {
+    // Check for MIDI messages from each Mega
+    checkMidiMessages(MEGA1_ADDRESS);
+    checkMidiMessages(MEGA2_ADDRESS);
+    checkMidiMessages(MEGA3_ADDRESS);
+    delay(10);  // Small delay for stability
+}
+
+void checkMidiMessages(uint8_t slaveAddress) {
+    Wire.beginTransmission(slaveAddress);
+    Wire.write(0x00);  // Command to request MIDI message from the slave
+    Wire.endTransmission();
+
+    Wire.requestFrom(slaveAddress, 3); // Request 3 bytes (MIDI message)
+    if (Wire.available() == 3) {
+        uint8_t status = Wire.read();
+        uint8_t data1 = Wire.read();
+        uint8_t data2 = Wire.read();
+        
+        // Send the MIDI message to USB
+        MidiUSB.sendMIDI(status, data1, data2);
+        MidiUSB.flush();
+    }
+}
